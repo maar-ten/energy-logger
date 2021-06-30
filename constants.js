d5fb9cdcb090dfebb6b30c0da8505a91bb46ce30
maar-ten <maar-ten@users.noreply.github.com>
2021-06-30 20:40:35 +0000

    Add rule for timestamp

---
+++ constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DSMR_OBIS_KEYS : {
+        timestamp: '0-0:1.0.0',
+        receivedTariff1: '1-0:1.8.1',
+        receivedTariff2: '1-0:1.8.2',
+        tariffIndicator: '0-0:96.14.0',
+        power: '1-0:1.7.0'
+    }
+}
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const { fromEvent } = require('rxjs');
 const { filter, map } = require('rxjs/operators');
+const { DSMR_OBIS_KEYS } = require('./constants');
 
-const regexpWattage = /1-0:1\.7\.0\(([0-9]*\.[0-9]*)\*kW\)/;
+const regexpPower = /1-0:1\.7\.0\(([0-9]*\.[0-9]*)\*kW\)/;
+const regexpTimestamp = /([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/;
 
 const port = new SerialPort('/dev/ttyUSB0', {
     baudRate: 115200,
@@ -12,16 +14,30 @@ const port = new SerialPort('/dev/ttyUSB0', {
 const parser = port.pipe(new Readline());
 
 const rows$ = fromEvent(parser, 'data');
-const wattage$ = rows$.pipe(
-    filter(row => row.startsWith('1-0:1.7.0')),
-    map(parseWattageFromRow)
+const power$ = rows$.pipe(
+    filter(row => row.startsWith(DSMR_OBIS_KEYS.power)),
+    map(parsePowerFromRow)
 );
-wattage$.subscribe(wattage => console.log(wattage + ' Watt'));
+power$.subscribe(power => console.log(power + ' Watt'));
 
-function parseWattageFromRow(row) {
-    const match = regexpWattage.exec(row);
+function parsePowerFromRow(row) {
+    const match = regexpPower.exec(row);
     if (match) {
         return parseFloat(match[1]) * 1000;
+    }
+}
+
+const timestamp$ = rows$.pipe(
+    filter(row => row.startsWith(DSMR_OBIS_KEYS.timestamp)),
+    map(parseTimeFromRow)
+);
+timestamp$.subscribe(console.log);
+
+function parseTimeFromRow(row) {
+    const match = regexpTimestamp.exec(row);
+    if (match) {
+        const timestamp = `20${match[1]}-${match[2]}-${match[3]}T${match[4]}:${match[5]}:${match[6]}`;
+        return new Date(timestamp).toString();
     }
 }
 
