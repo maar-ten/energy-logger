e67ead072840ece73fa4dee885ba26b0414cbb92
maar-ten <maar-ten@users.noreply.github.com>
2025-04-27 14:18:38 +0000

    fix git revert artifact

--- dsmr-client/dsmr-message-parser.js
+++ dsmr-client/dsmr-message-parser.js
@@ -60,58 +60,6 @@ class ObisParser {
     }
 }
 
-<<<<<<< HEAD
-class Timestamp {
-    constructor() {
-        this.offsetStr = this.createTimezoneOffsetStr();
-        this.parser = this.createTimestampParser();
-
-        console.log(`The timezone offset is: ${this.offsetStr}`);
-    }
-
-    /**
-     * DSMR timestamps do not contain timezone information.
-     * This causes the timestamp to be treated as UTC, which might not be correct.
-     * This function creates a timezone string from the system which will be added to the DSMR timestamp.
-     * 
-     * NOTE: If the system and the meter do not switch to DST at the same moment this might cause faulty data points.
-     */
-    createTimezoneOffsetStr() {
-        const offsetMinutes = new Date().getTimezoneOffset();
-        const hours = Math.abs(Math.floor(offsetMinutes / 60));
-        const hoursPadded = hours >= 10 ? `${hours}` : `0${hours}`;
-        const minutes = offsetMinutes % 60;
-        const minutesPadded = minutes >= 10 ? `${minutes}` : `0${minutes}`;
-        const sign = Math.sign(offsetMinutes) >= 0 ? '+' : '-';
-        
-        return `${sign}${hoursPadded}:${minutesPadded}`;
-    }    
-
-    createTimestampParser() {
-        return new ObisParser({
-            key: DSMR_OBIS_CODES.timestamp,
-            name: DSMR_OBIS_NAMES.timestamp,
-            matcher: /\(([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})[SW]{1}\)/, // matches '(210705164046S and ...W when winter time is active)'
-            parseValue: match => new Date(`20${match[1]}-${match[2]}-${match[3]}T${match[4]}:${match[5]}:${match[6]}`)
-        });
-    }
-
-    is(str) {
-        return this.parser.is(str);
-    }
-
-    parse(str) {
-        const {key, value} = this.parser.parse(str);
-        const timestamp = new Date(value + this.offsetStr);
-        return {
-            key, 
-            value: timestamp
-        };
-    }
-}
-
-=======
->>>>>>> parent of 1cc1cb4 (add timezone to timestamp)
 class ReceivedTariff {
     regex = /\(([0-9]*\.[0-9]*)\*kWh\)/; // matches '(002810.380*kWh)'
 
